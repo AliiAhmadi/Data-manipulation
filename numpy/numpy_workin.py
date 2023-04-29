7b15aef78054a296f9e4fb19ca0189d637619f55
aliiahmadi <aliahmadi.github@gmail.com>
2023-04-29 17:59:30 +0330

    slicing

--- numpy/numpy_workin.py
+++ numpy/numpy_workin.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-test_arr = np.array([3.7, -1.2, -2.6, 0.5, 12.9, 10.1])
+test_arr = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
 
 # print(test_arr.dtype)
 
@@ -14,6 +14,17 @@ test_arr = np.array([3.7, -1.2, -2.6, 0.5, 12.9, 10.1])
 # print(test_range)
 
 
-empty = np.empty((3, 5), dtype=np.uint32)
+# empty = np.empty((3, 5), dtype=np.uint32)
 
-print(empty.dtype)
+# print(empty.dtype)
+
+test_arr2 = np.array([[11, 12, 13, 14, 15], [16, 17, 18, 19, 20]])
+
+# print(test_arr)
+# print(test_arr2)
+# print(test_arr < test_arr2)
+
+test_arr[0][2:12] = 10000
+# test_arr[0][:] = 10000
+
+print(test_arr)
