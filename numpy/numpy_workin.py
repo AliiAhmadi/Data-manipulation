14fb4203d8f420527357cf46dc1e850909f3ad02
aliiahmadi <aliahmadi.github@gmail.com>
2023-04-29 18:57:12 +0330

    boolean indexing

--- numpy/numpy_workin.py
+++ numpy/numpy_workin.py
@@ -27,4 +27,50 @@ test_arr2 = np.array([[11, 12, 13, 14, 15], [16, 17, 18, 19, 20]])
 test_arr[0][2:12] = 10000
 # test_arr[0][:] = 10000
 
-print(test_arr)
+# print(test_arr)
+
+arr3d = np.array([[1, 2, 3],
+                  [4, 5, 6],
+                  [7, 8, 9]])
+
+# print(arr3d)
+
+# arr3d[:, 1] = 12
+# print(arr3d)
+
+
+names = np.array(['Bob', 'Joe', 'Will', 'Bob', 'Will', 'Joe', 'Joe'])
+
+# print(names)
+
+
+data = np.random.randn(7, 4)
+
+# print(data)
+
+# print(names == "Will")
+
+# print(names == "Bob")
+# print("-----------------------------")
+# print(data[names == 'Bob'])
+# print("-----------------------------")
+# print(data)
+
+# print(names != "Bob")
+cond = names == 'Bob'
+# print(cond)
+
+# print(data[~cond])
+
+
+second_test = (names == "Bob") | (names == "Will")
+
+# print(second_test)
+
+
+print(data)
+
+print("----------------------------")
+data[data < 0] = 0
+
+print(data)
