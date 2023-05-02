e2cd97fcf7178c862e976caabc6b7467408b7dd8
aliiahmadi <aliahmadi.github@gmail.com>
2023-05-02 22:14:15 +0330

    binary search

--- numpy/ufuncs.py
+++ numpy/ufuncs.py
@@ -16,6 +16,27 @@ remainders, whole_part = np.modf(arr)
 
 # # print(np.sqrt(whole_part))
 
-temp = np.sqrt(arr)
+# temp = np.sqrt(arr)
 
-print(np.isnan(temp))
+# print(np.isnan(temp))
+
+
+def binary_search(array, item):
+    func_data = {
+        "low": 0,
+        "high": len(array) - 1
+    }
+    
+    while func_data["high"] >= func_data["low"]:
+        middle = ((func_data["high"] + func_data["low"]) // 2)
+        guess = array[middle]
+        if guess == item:
+            return middle
+        elif guess > item:
+            func_data["high"] = middle - 1
+        elif guess < item:
+            func_data["low"] = middle + 1
+    return None
+
+
+print(binary_search([1, 2, 3, 4, 5, 6, 7], 34))
